9077f44afd85e4c5450d9feeeb91ed7b52b0006e
VictorSanchez94 <vs_zgz94@hotmail.com>
2015-11-16 18:34:16 +0100

    Plane.java

--- src/shapes/Plane.java
+++ src/shapes/Plane.java
@@ -21,10 +21,36 @@ public class Plane extends Obj3D {
 		this.b = b;
 		this.c = c;
 		this.d = d;
-		this.normal = new Vector(a, d).normalize();
+		Vector aux1 = new Vector(a,b);
+		Vector aux2 = new Vector(a,c);
+		this.normal = aux1.cross(aux2);
 	}
 
+	/**
+	 * Pre: ---
+	 * Post: This method returns a RayHit object with the point where ray intersects
+	 * 		 with the plane.
+	 * 
+	 */
 	public RayHit intersect(Ray ray) {
-		
+		if(ray.getDirection().dot(normal) != 0){	//The ray intersect with plane
+			Vector vAux = new Vector(a.substraction(ray.getOrigin()));
+			double lambda = vAux.dot(normal)/ray.getDirection().dot(normal);
+			return new RayHit(ray,normal,ray.getPoint(lambda));
+		}else{
+			return null;
+		}		
 	}
+	
+	public static void main (String[] args) {
+		Ray r = new Ray(new Point(2,3,0), new Vector(1.0,0.0,0.0));
+		Plane p = new Plane(new Point(4,4,-2), new Point(4,2,-2), new Point(4,4,2), new Point(4,2,2));
+		RayHit rh = p.intersect(r);
+		if(rh == null){
+			System.out.println("NULL");
+		}else{
+			System.out.println(rh.toString());
+		}
+	}
+	
 }
